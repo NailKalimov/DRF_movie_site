76ed466d4c84c12089b53aec5ad213c829d585af
Nail <nailkalimov@gmail.com>
2025-01-27 23:10:49 +0400

    initial commit. Movie serializer

---
+++ django_movie/movies/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Movie
+from .serializer import MovieListSerializer, MovieDetailSerializer
+
+
+class MovieListView(APIView):
+    def get(self, request):
+        movies = Movie.objects.filter(draft=False)
+        serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data)
+
+class MovieDetailView(APIView):
+    def get(self, request, pk):
+        movie = Movie.objects.get(pk=pk, draft=False)
+        serializer = MovieDetailSerializer(movie)
+        return Response(serializer.data)
